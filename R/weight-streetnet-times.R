81ea7bcbbb7d7f9ca58c2ded5caf3287817c9682
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2023-10-23 17:10:26 +0000

    slope

--- R/weight-streetnet-times.R
+++ R/weight-streetnet-times.R
@@ -49,7 +49,7 @@ sc_edge_dist <- function (graph) {
     xy1 <- as.data.frame (graph [, c (".vx1_x", ".vx1_y")])
     graph$d <- geodist::geodist (xy0, xy1, paired = TRUE, measure = "geodesic")
     if (".vx0_z" %in% names (graph) && ".vx1_z" %in% names (graph)) {
-        graph <- dplyr::mutate (graph, "dz" = .vx1_z - .vx0_z) %>%
+        graph <- dplyr::mutate (graph, "dz" = (.vx1_z - .vx0_z)/d) %>%
             dplyr::select (-c (.vx0_z, .vx1_z))
     }
     return (graph)
